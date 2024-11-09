6225498f9f52f8f0c709c0d558cc29f9d36227bb
koayck <116639605+koayck@users.noreply.github.com>
2024-11-09 16:42:26 +0800

    add .gitignore to ignore output folder and CMakePresets.json

--- out/build/my preset/CMakeFiles/CPT316-Simple-Compiler.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CPT316-Simple-Compiler.
-# This may be replaced when dependencies are built.
--- out/build/my preset/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/kck03/Desktop/CPT316-Simple-Compiler
-# Build directory: C:/Users/kck03/Desktop/CPT316-Simple-Compiler/out/build/my preset
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
